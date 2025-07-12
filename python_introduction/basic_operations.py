a84e617a7d29ee3f5db9d3f547c095c25ef142b5
Hanna Desalegn <hannadesalegn22@gmail.com>
2025-07-12 23:56:33 +0300

    arithmetic operations in Python by performing predefined calculations.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 =5
+
+sum = number1 + number2
+dif = number1 - number2
+mul = number1 * number2
+
+print("Addition of 10 and 5 is ",sum)
+print("Subtraction of 10 and 5 is ",dif)
+print("Multiplication of 10 and 5 is ",mul)
