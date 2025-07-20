697ab63fe621b911a25b9522dd901ddfe9595b6e
Hanna Desalegn <hannadesalegn22@gmail.com>
2025-07-20 20:58:45 +0300

    pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -5,8 +5,10 @@ while size < 1:
     print("Size must be a positive integer. Please try again.")
     size = input("Enter the size of the pattern: ")
     size = int(size)
-for i in range(size):
+
+i = 0
+while i < size:
     for j in range(size):
         print("*", end="")
     print()
-      
+    i += 1
